300140e4ba75a5a04d17dc41fae5abf920066f72
Sebastian <juans.guzmanf@gmail.com>
2022-12-09 16:27:55 -0500

    first commit

---
+++ interpolacion_lagrange.py
@@ -0,0 +1,10 @@
+# importamos la libreria numpy para hacer calculos 
+import numpy as np 
+#desarrolllo de la forma algebraica del polinomio 
+import sympy as sym
+#graficas 
+import matplotlib.pyplot as plt
+#ingresamos los datos de prueba 
+xi=np.array([0, 0.2, 0.3, 0.4])
+fi=np.array([1, 1.6, 1.7, 2.0])
+
