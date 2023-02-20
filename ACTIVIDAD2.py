1e6f6072956a7c341436c4922901ac10e0f6368a
Sebastian <juans.guzmanf@gmail.com>
2023-02-19 20:47:09 -0500

    second commit

--- ACTIVIDAD2.py
+++ ACTIVIDAD2.py
@@ -5,22 +5,27 @@
 #Importamos las librerias necesarias 
 import numpy as np
 import matplotlib.pylab as plt
+from scipy import integrate
 from scipy import signal as sp
-
-# Importamos todo el modulo sympy
-from sympy import *
-
+#from sympy import * 
+import sympy as sym
 # Importamos las variables simbolicas 'n' y 't'
 from sympy.abc import n, t
 
 # Periodo
-#T = pi
+Tmax=3
+Tmin=-3
+T = Tmax-Tmin
+print('el valor del periodo es:',T)
 
 # Frecuencia angular
-w = (2*pi)/T
+w = (2*np.pi)/T
+print('el valor de la frecuencia angular es:',w)
 amplitude = 1
-time = np.arange(-1, 10, 0.001)
-squareWaveFunction = (sp.square(2*time) * amplitude/2.0) + amplitude/2.0
+
+
+time = np.arange(-3, 3, 0.001)
+squareWaveFunction = sym.Piecewise((-1,((t>=-T/2) & (t<=-T/4))))
 
 # Graficamos la onda cuadrada
 plt.plot(time, squareWaveFunction, lw=2)
@@ -29,26 +34,7 @@ plt.annotate('T', xy = (np.pi, 0), xytext = (np.pi, -0.01))
 plt.annotate('T/2', xy = (np.pi / 2.0, 0), xytext = (np.pi / 2.0, 1.01))
 plt.ylabel('Amplitude')
 plt.xlabel('time(t)')
+plt.show()
 
 
-# Integramos la funcion (2/T) cuya variable es 't'
-# y limites de integracion son 0 y pi/2
 
-ao = 2/T*integrate(1, (t, 0, T/2))
-
-print("a0 = ")
-pprint(ao)
-
-# Integramos la funcion (2/pi)*cos(2nt) 
-# Su variable es 't' y sus limites de integracion son 0 y pi/2
-an = (2/T)*integrate(cos(n*w*t), (t, 0, T/2))
-
-print("an = ")
-pprint(an)
-
-# Integramos la funcion (2/pi*sin(2nt)
-# Su variable es 't' y sus limites de integracion son 0 y pi/2. 
-bn = (2/T)*integrate(sin(n*w*t), (t, 0, T/2))
-
-print("bn = ")
-pprint(bn)
